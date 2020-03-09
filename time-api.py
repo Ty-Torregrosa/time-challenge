06a082d9cf4e2a54829bd28a976203ea16d5bf45
Ty Torregrosa <tytorregrosa@gmail.com>
2020-03-09 14:01:01 -0400

    mvp flask api

---
+++ time-api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
+from datetime import datetime
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+class Time(Resource):
+    def get( self):
+        result = {'version': 'v1'}
+        result['time'] = datetime.now().time().__str__()
+        return jsonify(result)
+
+api.add_resource(Time, "/time", "/time/")
+
+if __name__ == '__main__':
+    app.run(debug=True)
