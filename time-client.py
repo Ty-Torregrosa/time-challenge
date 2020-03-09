b9c1271a3284c75eb5859d0811dfff128a3c37cf
Ty Torregrosa <tytorregrosa@gmail.com>
2020-03-09 14:19:19 -0400

    mvp time client

---
+++ time-client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python        
+import requests, time
+
+def do_loop(hz, dofor):
+    outputs = []
+    times_to_run = hz * dofor
+    url = "http://localhost:5000/time"
+    
+    
+    for i in range(times_to_run):
+        time.sleep(1/hz)
+        r = requests.get(url)
+        outputs += [{"status": r.status_code, "ttlb": r.elapsed.total_seconds()}]
+    return outputs
+
+
+def main():
+    print(do_loop(10, 1))
+    
+if __name__ == "__main__":
+    main()
